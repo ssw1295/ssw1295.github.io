041c68d61bb92e216dd014b78a5c293d5bc4fe16
서상원 <swseo@curioud.com>
2024-07-06 00:42:05 +0900

    웹팩으로 빌드 워치하여 실시간으로 지킬에서 확인할 수 있게 함

---
+++ react/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx', // TypeScript 진입점
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // 파일 확장자 처리
+  },
+  output: {
+    path: path.resolve(__dirname, '../jekyll/react-build'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/react-build/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // TypeScript 및 TSX 파일 처리
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            { source: '../jekyll/react-build/index.html', destination: path.resolve(__dirname, '../jekyll/_includes/react-index.html') },
+          ],
+        },
+      },
+    }),
+  ],
+  watch: true,
+};
