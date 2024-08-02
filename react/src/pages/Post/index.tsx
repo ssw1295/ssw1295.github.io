412fb0595cc55652c2a34a7be04c09105718e998
서상원 <swseo@curioud.com>
2024-08-02 17:07:07 +0900

    여러 리소스들의 확장자들 임포트할때 타입 에러 없앰

--- react/src/pages/Post/index.tsx
+++ react/src/pages/Post/index.tsx
@@ -1,7 +1,33 @@
-export default ({
+import './index.css'
 
-}) => (
-  <div>
-    hello post
-  </div>
-)
+import cat from './cat.png'
+
+export default ({
+  content,
+}: {
+  content: Element
+}) => {
+  return (
+    <div className="App">
+      <>
+        <div
+          dangerouslySetInnerHTML={{__html: content.outerHTML}}
+        ></div>
+      </>
+      <header className="App-header">
+        <img src={ cat }/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload. This is a test.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
